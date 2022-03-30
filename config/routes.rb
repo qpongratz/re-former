742e290ec30da6786b0ea16f69f0a031fbc43fb4
Quentin Pongratz <qpongratz@gmail.com>
2022-03-30 16:18:09 -0500

    Add basic index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users, only: [:new, :create]
+  root 'users#index'
+  resources :users
 end
